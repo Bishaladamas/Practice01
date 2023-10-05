bac5dc6a9709a0696a9578870fc4c4624ef86177
Bishal Debroy <bishal1.debroy@stu.adamasuniversity.ac.in>
2023-10-05 16:57:45 +0530

    TEST

---
+++ Sum.c
@@ -0,0 +1,15 @@
+//
+// Created by bisha on 05-10-2023.
+//
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("ENTER THE VALUE OF A: ");
+    scanf("%d",&a);
+    printf("ENTER THE VALUE OF B: ");
+    scanf("%d",&b);
+
+    int c=a+b;
+    printf("THE SUM IS: %d",c);
+}
