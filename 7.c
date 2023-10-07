cff400543e5f91bba0186840efd13ea3df2009c8
Bishal Debroy <bishal1.debroy@stu.adamasuniversity.ac.in>
2023-10-07 15:49:44 +0530

    test

---
+++ 7.c
@@ -0,0 +1,12 @@
+//
+// Created by bisha on 07-10-2023.
+//Write a program to convert the value of temperature from degree celcius to degree fahrenheit.
+#include<stdio.h>
+int main()
+{
+    float c,f;
+    printf("Enter the temperature in celcius :\n");
+    scanf("%f",&c);
+    f=(c*9/5)+32;
+    printf("%f\n",f);
+}
