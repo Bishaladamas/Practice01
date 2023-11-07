34bb4b36bf0dffb115d115d1160119954962fee1
Bishal Debroy <bishal1.debroy@stu.adamasuniversity.ac.in>
2023-11-07 23:49:23 +0530

    c programing

---
+++ 48.c
@@ -0,0 +1,20 @@
+//
+// Created by bisha on 07-11-2023.
+//Write a program in c to print the pattern
+A
+        B B
+        C C C
+D D D D
+E E E E E
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<=5;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            printf("%c",'A'-1+i);
+        }
+        printf("\n");
+    }
+}
