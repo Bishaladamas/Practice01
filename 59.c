492448a53328cc57e13f73ecf6359ed6ec097f43
Bishal Debroy <bishal1.debroy@stu.adamasuniversity.ac.in>
2023-11-08 20:22:10 +0530

    CHECK

---
+++ 59.c
@@ -0,0 +1,12 @@
+//
+// Created by bisha on 08-11-2023.
+//WRITE A PROGRAM IN C TO PRINT THE ASCII VALUE FOR EVERY CHARACTER IN A STRING.
+#include<stdio.h>
+int main ()
+{
+    char ch;
+    printf("ENTER ANY CHARACTER : ");
+    scanf("%c",&ch);
+    printf("%d",&ch);
+    return 0;
+}
