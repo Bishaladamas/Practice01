aac7af0129f0b207c7a6a85a7f2fceac412433f7
Bishal Debroy <bishal1.debroy@stu.adamasuniversity.ac.in>
2023-11-08 20:37:52 +0530

    CHECK

---
+++ 61.c
@@ -0,0 +1,3 @@
+//
+// Created by bisha on 08-11-2023.
+//writne a prigram in c to create an array of strings to store the name of your five best friends and display them in decending order fot the index
